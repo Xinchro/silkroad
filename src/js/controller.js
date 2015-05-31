6c39fb965e2fb25a2357374aff54a4a0a55d16f6
Xinchro <TheXrocks@gmail.com>
2015-05-31 01:33:31 +0100

    Changed the way the systems/stops system works, using objects now instead

--- src/js/controller.js
+++ src/js/controller.js
@@ -6,21 +6,14 @@ module.controller('silkRoadController', ["$scope", function($scope) {
   $scope.stations = ['stationA', 'stationB', 'stationC'];
   $scope.commodities = ['commodityA', 'commodityB', 'commodityC'];
 
-  $scope.stop = {};
-  $scope.stops = [];
-  $scope.prevStop;
-  $scope.currentStop;
-  $scope.nextStop;
+  $scope.currentStop = {};
 
   $scope.stopNo = 1;
   $scope.noOfStops = 3;
   
-  $scope.currentSystem = $scope.systems[$scope.stopNo-1];
-  $scope.currentStation = $scope.stations[$scope.stopNo-1];
-  $scope.currentBuy = $scope.commodities[$scope.stopNo-1];
-  $scope.currentSell = $scope.commodities[$scope.stopNo-1+1];
-  
-  $scope.nextSystem = $scope.systems[$scope.stopNo-1+1];
+  $scope.nextSystem = $scope.systems[$scope.stopNo-1];
+
+  $scope.stops = [];
 
   $scope.prevStop = function() {
     if($scope.stopNo > 1) {
@@ -28,9 +21,7 @@ module.controller('silkRoadController', ["$scope", function($scope) {
     } else {
       $scope.stopNo = $scope.noOfStops;
     }
-    $scope.currentSystem = $scope.systems[$scope.stopNo-1];
-    $scope.nextSystem = $scope.systems[$scope.stopNo-1+1];
-    console.log("butts " + $scope.stopNo);
+    $scope.currentStop = $scope.stops[$scope.stopNo-1];
   };
 
   $scope.nextStop = function() {
@@ -39,9 +30,53 @@ module.controller('silkRoadController', ["$scope", function($scope) {
     } else {
       $scope.stopNo = 1;
     }
-    $scope.currentSystem = $scope.systems[$scope.stopNo-1];
-    $scope.nextSystem = $scope.systems[$scope.stopNo-1+1];
-    console.log("butts2 " + $scope.stopNo);
+    $scope.currentStop = $scope.stops[$scope.stopNo-1];
   };
+
+  $scope.getStops = function() {
+    return $scope.stops;
+  };
+
+  /**
+    * List of stops
+    1*
+    * {system, station, buy, sell, prevStop, nextStop}
+    **/
+
+  $scope.stop1 = {
+    system: $scope.systems[0], 
+    station: $scope.stations[0], 
+    buy: $scope.commodities[0], 
+    sell: $scope.commodities[1],
+    prevStop: {},
+    nextStop: {}
+  };
+  $scope.stop2 = {
+    system: $scope.systems[1], 
+    station: $scope.stations[1], 
+    buy: $scope.commodities[1], 
+    sell: $scope.commodities[2],
+    prevStop: {},
+    nextStop: {}
+  };
+  $scope.stop3 = {
+    system: $scope.systems[2], 
+    station: $scope.stations[2], 
+    buy: $scope.commodities[2], 
+    sell: $scope.commodities[0],
+    prevStop: {},
+    nextStop: {}
+  };
+
+  $scope.stops = [$scope.stop1, $scope.stop2, $scope.stop3];
+  
+  $scope.stop1.prevStop = $scope.stops[2];
+  $scope.stop1.nextStop = $scope.stops[1];
+  $scope.stop2.prevStop = $scope.stops[0];
+  $scope.stop2.nextStop = $scope.stops[2];
+  $scope.stop3.prevStop = $scope.stops[1];
+  $scope.stop3.nextStop = $scope.stops[0];
+
+  $scope.currentStop = $scope.stops[0];
 
 }]);
